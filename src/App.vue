27ca0a67a5c6e1b6799395830b36b702e54657b3
jnavarro30 <jnavarro30@live.com>
2023-06-19 15:40:54 -0700

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+  nav {
+    background: red;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  a {
+    padding: 1rem;
+    color: white;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.config.globalProperties.state = {
+    color: 'red'
+}
+
+app.use(router)
+
+app.mount('#app')
